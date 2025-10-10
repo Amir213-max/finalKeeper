89fa1064d2edfa50fdf54fc094caadae1324e4c8
Amirgharib <amirgharib593@gmail.com>
2025-10-10 19:33:38 +0300

    Fixed Sidebar import path

--- app/Componants/navbar.js
+++ app/Componants/navbar.js
@@ -6,7 +6,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import CartSidebar from './CartSidebar';
 import SearchComponent from './SearchComponant';
-import Sidebar from './Sidebar';
+import Sidebar from './sidebar';
 import NavbarNotifications from './NotificationsBell';
 
 export default function NavbarWithLinks({ onSelectCategory }) {
